3f030994cd5399ca343529b5a4bdc853ec176bbb
maeda-kota <kota_maeda@demia.co.jp>
2025-12-09 23:18:31 +0900

    教材修正に伴う微修正

--- static/js/game.js
+++ static/js/game.js
@@ -1,25 +1,28 @@
+//1
 document.addEventListener("DOMContentLoaded",() => {
-    let startFlag = 0; // 0→開始前、1→ゲーム中、2→終了
+
+    //2
+    let startFlag = 0; // 0→開始前、１→開始待機、２→ゲーム中、３→終了
     let startTime;
     let missTypeCount = 0;
     let typeCount = 0;
     let current = 0;
     let letterCount= 0;
-    
-    //効果音
+
+    //3
     const clearSound = document.getElementById("type_clear");
     const missSound = document.getElementById("type_miss");
     const countSound = document.getElementById("count_down");
     const startSound = document.getElementById("start_sound");
 
-    //フィッシャー・イェーツのシャッフル (Fisher-Yates Shuffle)    
+    //4
     function shuffleArray(array) {
         for (let i = array.length - 1; i > 0; i--) {
             // 0からiまでのランダムなインデックスを生成
             const j = Math.floor(Math.random() * (i + 1));
             // array[i] と array[j] を入れ替える
             [array[i], array[j]] = [array[j], array[i]];
-        }
+        };
         return array;
     };
 });
